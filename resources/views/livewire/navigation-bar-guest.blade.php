cfb2c33657af3d57d03b2dc86215899c9aaeeff8
dendydandees <dendydharmawanq@gmail.com>
2021-03-13 19:00:44 +0700

    edit welcome page

--- resources/views/livewire/navigation-bar-guest.blade.php
+++ resources/views/livewire/navigation-bar-guest.blade.php
@@ -1,11 +1,11 @@
-<nav x-data="{ open: false }" class="bg-gray-800">
+<nav x-data="{ open: false }" class="bg-white shadow-sm">
     <div class="container sm:px-6 lg:px-8">
         <div class="max-w-7xl mx-auto">
           <div class="relative flex items-center justify-between h-16">
             @if (Route::has('login'))
               <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 <!-- Mobile menu button-->
-                <button @click="open = !open" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                <button @click="open = !open" type="button" class="inline-flex items-center justify-center p-2 rounded-md hover:text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                   <span class="sr-only">Open main menu</span>
                     <!-- if hidden -->
                     <svg :class="{'hidden' : open, 'block' : !open}" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -31,15 +31,15 @@
                 <div class="hidden sm:block sm:ml-6">
                   <div class="flex space-x-4">
                     @auth
-                      <a href="{{ route('dashboard') }}" class="{{ Route::is('dashboard') ? 'bg-gray-900 text-white' : 'text-gray-300' }} px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white">
+                      <a href="{{ route('dashboard') }}" class="{{ Route::is('dashboard') ? 'bg-indigo-600 text-white' : '' }} px-3 py-2 rounded-md text-sm font-medium hover:bg-indigo-700 hover:text-white">
                           {{ __('Dashboard') }}
                       </a>
                     @else
-                      <a href="{{ route('login') }}" class="{{ Route::is('login') ? 'bg-gray-900 text-white' : 'text-gray-300' }} px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white">
+                      <a href="{{ route('login') }}" class="{{ Route::is('login') ? 'bg-indigo-600 text-white' : '' }} px-3 py-2 rounded-md text-sm font-medium hover:bg-indigo-700 hover:text-white">
                           {{ __('Sign In') }}
                       </a>
                       @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="{{ Route::is('register') ? 'bg-gray-900 text-white' : 'text-gray-300' }} px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white">
+                        <a href="{{ route('register') }}" class="{{ Route::is('register') ? 'bg-indigo-600 text-white' : '' }} px-3 py-2 rounded-md text-sm font-medium hover:bg-indigo-700 hover:text-white">
                           {{ __('Sign Up') }}
                         </a>
                       @endif
@@ -55,11 +55,11 @@
           <div :class="{'block' : open, 'hidden' : !open}" class="transition-all duration-500 ease-in-out sm:hidden" id="mobile-menu">
             <div class="pt-2 pb-3 space-y-1">
               @auth
-                <a href="{{ route('dashboard') }}" class="{{ Route::is('dashboard') ? 'bg-gray-900 text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ __('Dashboard') }}</a>
+                <a href="{{ route('dashboard') }}" class="{{ Route::is('dashboard') ? 'bg-indigo-600 text-white' : '' }} hover:bg-indigo-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ __('Dashboard') }}</a>
               @else
-                <a href="{{ route('login') }}" class="{{ Route::is('login') ? 'bg-gray-900 text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ __('Sign In') }}</a>
+                <a href="{{ route('login') }}" class="{{ Route::is('login') ? 'bg-indigo-600 text-white' : '' }} hover:bg-indigo-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ __('Sign In') }}</a>
                 @if (Route::has('register'))
-                  <a href="{{ route('register') }}" class="{{ Route::is('register') ? 'bg-gray-900 text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ __('Sign up') }}</a>
+                  <a href="{{ route('register') }}" class="{{ Route::is('register') ? 'bg-indigo-600 text-white' : '' }} hover:bg-indigo-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ __('Sign up') }}</a>
                 @endif
               @endauth
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,38 @@
 <x-guest-layout>
-    <x-jet-welcome />
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-white overflow-hidden">
+  <div class="max-w-7xl mx-auto">
+    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+        <polygon points="50,0 100,0 50,100 0,100" />
+      </svg>
+
+      <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+      </div>
+
+      <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div class="sm:text-center lg:text-left">
+          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span class="block xl:inline">Data to enrich your</span>
+            <span class="block text-indigo-600 xl:inline">online business</span>
+          </h1>
+          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+          </p>
+          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                Get started
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
+  </div>
+</div>
+
 </x-guest-layout>
