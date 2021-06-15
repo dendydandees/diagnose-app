a68d199118489cd123733835a75776300922ed9f
dendydandees <dendydharmawanq@gmail.com>
2021-06-15 22:51:24 +0700

    change seeder not user factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,52 +17,76 @@ class UserSeeder extends Seeder
     public function run()
     {
         // create admin
-        User::factory(1)
-        ->withAdmin()
-        ->create([
+        // User::factory(1)
+        // ->withAdmin()
+        // ->create([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@diagnose.com',
+        //     'email_verified_at' => now(),
+        //     'password' => bcrypt('Diagnose21'),
+        //     'remember_token' => Str::random(10)
+        // ])
+        // ->each(function ($user) {
+        //     $user->assignRole('admin');
+        // });
+        $admin = User::create([
             'name' => 'Admin',
             'email' => 'admin@diagnose.com',
             'email_verified_at' => now(),
             'password' => bcrypt('Diagnose21'),
             'remember_token' => Str::random(10)
-        ])
-        ->each(function ($user) {
-            $user->assignRole('admin');
-        });
+        ]);
+        $admin->assignRole('admin');
 
         // create expert
-        User::factory(1)
-        ->withExpert()
-        ->create([
+        // User::factory(1)
+        // ->withExpert()
+        // ->create([
+        //     'name' => 'Nadya Puspita Ekawardhani, M. Psi',
+        //     'email' => 'nadyapuspita@personalgrowth.com',
+        //     'email_verified_at' => now(),
+        //     'password' => bcrypt('Personalgrowth21'),
+        //     'remember_token' => Str::random(10)
+        // ])
+        // ->each(function ($user) {
+        //     $user->assignRole('expert');
+        // });
+        $expert = User::create([
             'name' => 'Nadya Puspita Ekawardhani, M. Psi',
             'email' => 'nadyapuspita@personalgrowth.com',
             'email_verified_at' => now(),
             'password' => bcrypt('Personalgrowth21'),
             'remember_token' => Str::random(10)
-        ])
-        ->each(function ($user) {
-            $user->assignRole('expert');
-        });
+        ]);
+        $expert->assignRole('expert');
 
         // create user
-        User::factory(1)
-        ->withDendyProfile()
-        ->create([
+        // User::factory(1)
+        // ->withDendyProfile()
+        // ->create([
+        //     'name' => "Dendy Dharmawan",
+        //     'email' => "dendy@yopmail.com",
+        //     'email_verified_at' => now(),
+        //     'password' => bcrypt('Gemscool88'),
+        //     'remember_token' => Str::random(10),
+        // ])
+        // ->each(function ($user) {
+        //     $user->assignRole('user');
+        // });
+
+        // User::factory(2)
+        // ->withUserProfile()
+        // ->create()
+        // ->each(function ($user) {
+        //     $user->assignRole('user');
+        // });
+        $user = User::create([
             'name' => "Dendy Dharmawan",
             'email' => "dendy@yopmail.com",
             'email_verified_at' => now(),
             'password' => bcrypt('Gemscool88'),
             'remember_token' => Str::random(10),
-        ])
-        ->each(function ($user) {
-            $user->assignRole('user');
-        });
-
-        User::factory(2)
-        ->withUserProfile()
-        ->create()
-        ->each(function ($user) {
-            $user->assignRole('user');
-        });
+        ]);
+        $user->assignRole('user');
     }
 }
